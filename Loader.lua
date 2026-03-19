b477efb022dbbaa1b8d4ed970022d9b8f33a9dbe
umrfyn <100963276+umrfyn@users.noreply.github.com>
2026-03-19 23:09:12 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,5 @@
 local PlaceScripts = {
-    [155615604] = "https://raw.githubusercontent.com/4levy/Elystra.wtf/refs/heads/main/PL.lua",
-    [77837537595343] = "https://raw.githubusercontent.com/4levy/Elystra.wtf/refs/heads/main/BannaTown.lua",
-    [114116662845070] = "https://raw.githubusercontent.com/4levy/Elystra.wtf/refs/heads/main/CityBanna.lua",
-    [14807448145] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/ThaiBanCity.txt",
-    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/SilentAssassins.lua",
-    [89528376983973] = "https://raw.githubusercontent.com/umrfyn/Elystra.wtf/refs/heads/main/MazePVPOPENSRC.lua"
+    [0000000] = "https://raw.githubusercontent.com/umrfyn"
 }
 
 local url = PlaceScripts[game.PlaceId]
