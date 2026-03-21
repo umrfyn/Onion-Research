96e1879433838c60743306531014245dd3749e84
umrfyn <100963276+umrfyn@users.noreply.github.com>
2026-03-21 20:21:41 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,7 @@
 local PlaceScripts = {
-    [0000000] = "https://raw.githubusercontent.com/umrfyn"
+    [89528376983973] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/MazePVPOPENSRC.lua",
+    [14807448145] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/ThaiBanCity.lua",
+    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/SilentAssassins.lua"
 }
 
 local url = PlaceScripts[game.PlaceId]
