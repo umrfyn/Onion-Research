dad0ae1bbb6b3d69e9fbeee345a010d8444b468f
umrfyn <100963276+umrfyn@users.noreply.github.com>
2026-03-24 21:46:41 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ local KING_LEGACY_SCRIPT = "https://raw.githubusercontent.com/umrfyn/Onion-Resea
 local PlaceScripts = {
     [89528376983973] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/MazePVPOPENSRC.lua",
     [14807448145] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/ThaiBanCity.lua",
-    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/SilentAssassins.lua"
+    [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/SilentAssassins.lua",
 
     [4520749081] = KING_LEGACY_SCRIPT, -- Sea 1
     [6381829480] = KING_LEGACY_SCRIPT, -- Sea 2
