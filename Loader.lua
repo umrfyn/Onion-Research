9eeb3969ed73ffa90b12af1ac4c37be8a75fb6e5
umrfyn <100963276+umrfyn@users.noreply.github.com>
2026-03-24 21:45:10 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,14 @@
+local KING_LEGACY_SCRIPT = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/KingLegacy.lua"
+
+
 local PlaceScripts = {
     [89528376983973] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/MazePVPOPENSRC.lua",
     [14807448145] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/ThaiBanCity.lua",
     [103854444055060] = "https://raw.githubusercontent.com/umrfyn/Onion-Research/refs/heads/main/SilentAssassins.lua"
+
+    [4520749081] = KING_LEGACY_SCRIPT, -- Sea 1
+    [6381829480] = KING_LEGACY_SCRIPT, -- Sea 2
+    [15759515082] = KING_LEGACY_SCRIPT -- Sea 3
 }
 
 local url = PlaceScripts[game.PlaceId]
